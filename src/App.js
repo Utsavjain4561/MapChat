b83d2f138d1aaadced6fbc691c2fbc2cf7fefe82
Utsavjain4561 <utsavslife@gmail.com>
2020-05-06 21:45:26 +0530

    added leaflet map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as data from "./hotspots.json";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "./App.css";
+var myIcon = L.icon({
+  iconUrl:
+    "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png",
+  iconSize: [20, 20],
+  iconAnchor: [10, -20],
+  popupAnchor: [0, 0],
+});
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 22,
+      lng: 80,
+      zoom: 5,
+      userLocation: false,
+    };
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          zoom: 12,
+          userLocation: true,
+        });
+      },
+      (err) => {
+        console.log("Location Blocked by user");
+      },
+      { maximumAge: 10000, timeout: 5000, enableHighAccuracy: true }
+    );
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map className="map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.state.userLocation ? (
+          <Marker icon={myIcon} position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ) : (
+          ""
+        )}
+      </Map>
+    );
+  }
 }
-
-export default App;
